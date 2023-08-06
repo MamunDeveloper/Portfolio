99320ff24550a125b73e90d0057ee67c198bff37
Mamun <md0465981@gmail.com>
2023-08-06 21:50:21 +0600

    Fixed map width

--- app/contact/page.js
+++ app/contact/page.js
@@ -12,13 +12,13 @@ import {
 } from "react-icons/fa";
 
 function page() {
-
   return (
     <section id={styles.contactSection}>
       <h1 className="heading">Contact Us</h1>
       <div className="sepDiv flexBox">
         <div className="cards" id={styles.mapCard}>
           <iframe
+            id={styles.contactPageMap}
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7237.582020479406!2d91.86712863953116!3d24.905109342760113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3750552700bfdb4f%3A0x369ba0b482046794!2sAmbarkhana%2C%20Sylhet!5e0!3m2!1sen!2sbd!4v1689789418761!5m2!1sen!2sbd"
             width={250}
             height={200}
@@ -79,7 +79,7 @@ function page() {
       <br />
       <ContactForm />
       <div id="FaqBox">
-        <FAQSection/>
+        <FAQSection />
       </div>
     </section>
   );
